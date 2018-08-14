a64cf3318104301f67a4a7ed2f8a31061c6a69cc
Daniel Jones <danieljonesdmj@gmail.com>
2018-08-14 14:14:42 +0100

    Refactor Printing test name

--- spec/PrintingSpec.js
+++ spec/PrintingSpec.js
@@ -1,5 +1,5 @@
 describe('Printing', function() {
-  it('full account summary can be printed', function() {
+  it('can display full account summary', function() {
     printing = new Printing();
     printing.accountSummary = ['12/08/2018 || 50.00 ||   || 50.00', '13/08/2018 ||   || -10.00 || 40.00']
     expect(printing.printAccountSummary()).toEqual('date || credit || debit || balance ' + '\n' + '12/08/2018 || 50.00 ||   || 50.00' + '\n' + '13/08/2018 ||   || -10.00 || 40.00')
