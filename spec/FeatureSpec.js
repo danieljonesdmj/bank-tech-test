5eb3a002d3139a30287285cfbcb5bdfdc9032720
Daniel Jones <danieljonesdmj@gmail.com>
2018-08-15 14:46:52 +0100

    Refactor feature test syntax

--- spec/FeatureSpec.js
+++ spec/FeatureSpec.js
@@ -7,5 +7,5 @@ describe('Feature Test', function() {
     transaction.deposit('14/02/2018', 50);
     transaction.withdraw('15/02/2018', 20);
     expect(transaction.printer.printAccountSummary()).toEqual('date || credit || debit || balance ' + '\n' + '13/02/2018 || 100.00 ||   || 100.00' + '\n' + '14/02/2018 || 50.00 ||   || 150.00' + '\n' + '15/02/2018 ||   || -20.00 || 130.00');
-  })
+  });
 });
