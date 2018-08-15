84e820dcba3053f87a65dbaf6eecb9aea3fedf47
Daniel Jones <danieljonesdmj@gmail.com>
2018-08-15 17:42:30 +0100

    Refactor addTransactionRow to be two seperate methods

--- src/Transaction.js
+++ src/Transaction.js
@@ -9,29 +9,35 @@ Transaction.prototype.deposit = function(date, num) {
   this.transactionHistory.push(num);
   this.transactionDate.push(date);
   this._calculateBalance();
-  this._addTransactionRow();
+  this._addDepositRow();
 };
 
 Transaction.prototype.withdraw = function (date, num) {
   this.transactionHistory.push(-num);
   this.transactionDate.push(date);
   this._calculateBalance();
-  this._addTransactionRow();
+  this._addWithdrawRow();
 };
 
-Transaction.prototype._addTransactionRow = function () {
+Transaction.prototype._addDepositRow = function () {
   for (var i = 0; i < this.transactionDate.length; i++) {
-    if (this.transactionHistory[i] < 0) {
-      var debit = this.transactionDate[i] + ' ||   || ' + (this.transactionHistory[i] + '.00') + ' || ' + (this.account.accountBalance[i] + '.00');
-      this.printer.accountSummary.length = i;
-      this.printer.accountSummary.push(debit);
-    } else if ( this.transactionHistory[i] > 0) {
+    if ( this.transactionHistory[i] > 0) {
       var credit = this.transactionDate[i] + ' || ' + (this.transactionHistory[i] + '.00') + ' ||   || ' + (this.account.accountBalance[i] + '.00');
       this.printer.accountSummary.length = i;
       this.printer.accountSummary.push(credit);
     }
   }
 };
+
+Transaction.prototype._addWithdrawRow = function () {
+  for (var i = 0; i < this.transactionDate.length; i++) {
+    if (this.transactionHistory[i] < 0) {
+      var debit = this.transactionDate[i] + ' ||   || ' + (this.transactionHistory[i] + '.00') + ' || ' + (this.account.accountBalance[i] + '.00');
+      this.printer.accountSummary.length = i;
+      this.printer.accountSummary.push(debit);
+    };
+  }
+}
 
 Transaction.prototype._calculateBalance = function () {
   var sum = this.transactionHistory.reduce((a, b) => a + b, 0);
