184a17e6deaaf281678a5791e93d779a01b3ac06
Daniel Jones <danieljonesdmj@gmail.com>
2018-08-16 17:47:54 +0100

    Refactor feature test

--- spec/FeatureSpec.js
+++ spec/FeatureSpec.js
@@ -1,8 +1,6 @@
 describe('Feature Test', function() {
   it('should run a series of transactions and print the summary', function() {
     transaction = new Transaction();
-    account = new Account();
-    printer = new Printer();
     transaction.deposit('13/02/2018', 100);
     transaction.deposit('14/02/2018', 50);
     transaction.withdraw('15/02/2018', 20);
